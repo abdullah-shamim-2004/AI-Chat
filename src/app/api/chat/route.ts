98178f2efa784d249ea53d8558347e0074ca8589
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2026-03-16 22:04:51 +0600

    feau(chat): add Groq AI Streaming chat API route.

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,37 @@
+import { groq } from "@ai-sdk/groq";
+import { streamText } from "ai";
+
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+// main function
+export async function POST(req: Request) {
+  try {
+    // Get the message array from front-end
+    const { messages } = await req.json();
+    // validate the message
+    if (!messages || !Array.isArray(messages)) {
+      return new Response(
+        JSON.stringify({
+          error: "Invalid messages format",
+        }),
+        { status: 400 },
+      );
+    }
+    // call the groq and stream the response
+    const result = streamText({
+      model: groq("llama-3.3-70b-versatile"),
+      system:
+        "You are a helpful, friendly assistant. Give clear and concise answers.",
+      messages,
+    });
+    //Return the stream to the frontend
+    return result.toTextStreamResponse();
+  } catch (error) {
+    // console the error
+    console.error("[CHAT API ERROR]", error);
+    return new Response(
+      JSON.stringify({ error: "Something went wrong. Please try again." }),
+      { status: 500 },
+    );
+  }
+}
