ad07028974e1fe836f491189a39202486ffc5984
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2026-03-31 16:56:28 +0600

    feat(next-auth) : Add next auth configuration.

---
+++ src/lib/auth.ts
@@ -0,0 +1,33 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import Google from "next-auth/providers/google";
+import { db } from "./db";
+export const { handlers, signIn, signOut, auth  } = NextAuth({
+  adapter: PrismaAdapter(db),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+});
