5af4d9e0d536521e8ed22d5c62350a948468863b
abdullah-shamim-2004 <abdullahshamim884@gmail.com>
2026-04-12 23:13:41 +0600

    feat(sign_in) : Add sign in button and link on the header of the ui.

--- src/components/chat/chat-window.tsx
+++ src/components/chat/chat-window.tsx
@@ -7,6 +7,8 @@ import { useState } from "react";
 import { DefaultChatTransport } from "ai";
 import { Message } from "@/types";
 import MessageList from "./message-list";
+import { Button } from "../ui/button";
+import Link from "next/link";
 
 export default function ChatWindow() {
   const [input, setInput] = useState("");
@@ -52,9 +54,20 @@ export default function ChatWindow() {
   return (
     <div className="flex flex-col h-screen ">
       {/* Header */}
-      <div className="px-6 py-4 border-b border-gray-200">
-        <h2 className="text-lg font-semibold ">AI Chat</h2>
-        <p className="text-xs">Powered by Groq</p>
+      <div className="flex justify-between items-center px-6 py-4 border-b border-gray-200">
+        {/* Logo */}
+        <div className="">
+          <h2 className="text-lg font-semibold ">AI Chat</h2>
+          <p className="text-xs">Powered by Groq</p>
+        </div>
+        {/* Auth  section */}
+        <div className="flex items-center gap-2">
+          <Link href={"/login"}>
+            <Button variant="default" className="cursor-pointer">
+              Sign In
+            </Button>
+          </Link>
+        </div>
       </div>
       {/* Error Message */}
       {error && (
